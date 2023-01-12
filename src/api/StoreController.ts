1829498a66f2d15dfc4768d4cb9f31b3b5c50e62
Diney Oliveira <diney.oliveira>
2023-01-12 11:17:18 -0300

    configurando o projeto

---
+++ src/api/StoreController.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from "express";
+import { getRepository } from "typeorm";
+import { Store } from "../entity/Store";
+
+
+
+export async function save(req: Request, res: Response){
+    const storeRepository = getRepository(Store)
+
+    const savedStore = await storeRepository.save(req.body)
+
+    return res.status(200).json(savedStore);
+}
+
+export async function getAll(req: Request, res: Response){
+    const storeRepository = getRepository(Store);
+
+    const allStores = await storeRepository.find();
+
+    return res.json(allStores);
+    
+}
